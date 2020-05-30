c36cf045258d3689343f326d54f24264269c25d1
leomet07 <leomet07@gmail.com>
2020-05-30 11:18:28 -0400

    trim vid

--- edit.py
+++ edit.py
@@ -25,11 +25,21 @@ with open(os.path.join("src", "files.txt"), "w") as text_file:
 
                 text_file.write("file '" + file + "' \n")
 
+def compile(file_of_names):
+    print("\n\n**********Compiling....********************\n\n")
+    # after run, compile
+    file_of_names = file_of_names.strip()
+    os.system("cd src && ffmpeg -f concat -i " + str(file_of_names) +  " -c copy output.mkv && cd ..")
+    print("\n\n**********Done!********************\n\n")
 
-print("\n\n**********Compiling....********************\n\n")
-# after run, compile
-os.system("cd src && ffmpeg -f concat -i files.txt -c copy output.mkv && cd ..")
-print("\n\n**********Done!********************\n\n")
+def trim_vid(input_file, output_file, start_time, duration):
+    
+    input_file = input_file.strip()
+    output_file = output_file.strip()
+    start_time = start_time.strip()
+    duration = duration.strip()
+
+    os.system("ffmpeg -i " +  str(input_file) + " -ss " + str(start_time) + " -t " + str(duration) + " -c copy " + str(output_file))
 
 
-
+trim_vid("run.mkv", "out.mkv" , "00:00:05", "00:00:05")
